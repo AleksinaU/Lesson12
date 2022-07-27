8cc4191aebdf765e24b3180aab3b0f5b3ae677a0
Юлия Алексина <ulchez@mail.ru>
2022-07-27 17:23:34 +0300

    Добавьте файлы проекта.

---
+++ Lesson12/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12
+{
+    static class Circle
+    {
+        static public double GetSquare(double r)
+        {
+        return Math.PI * r*r;
+        }
+        static public double GetLenght(double r)
+        {
+            return 2*Math.PI * r;
+        }
+        static public string GetPoint(double r, double x0, double y0, double x, double y)
+        {
+            if ((x - x0) * (x - x0) + (y - y0) * (y - y0) == r * r)
+                return "Точка принадлежит кругу";
+            else
+                return "Точка не принадлежит кругу";
+        }
+    }
+}
---
+++ Lesson12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату x центра окружности");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату y центра окружности");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Задайте координату x точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Задайте координату y точки");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double square = Circle.GetSquare(r);
+            double lenght = Circle.GetLenght(r);
+            string point = Circle.GetPoint(r, x0, y0, x, y);
+            Console.WriteLine($"Площадь круга равна {square:f2} \nДлина окружности равна {lenght:f2} \n {point}");
+            Console.ReadKey();
+        }
+    }
+}
